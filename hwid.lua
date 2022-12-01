75320932d8d5a73853e74d3dd36a870a546a832a
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2022-12-01 08:43:44 +0330

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,16 +1,17 @@
 
 function DDFEDAFAA()
-     res1 = all_trim(hwid)
- hwid = string.gsub(res1,'%A+','')
+ hwid1 = all_trim(hwid)
+ hwid = string.gsub(hwid1,'%A+','')
  local hardwareid_verifier = hardwareid
-  if hardwareid_verifier = hwid then
-speak("fuck you")
-    --vampy_bypass()
-else
-
+  if hardwareid_verifier ~= hwid then
+          
 speak("fuck your mother")
 writeToClipboard(hardwareid)
 showMessage("please register as vip member baby ///send me your hwid copyed to your clipboard  ///@bravo6ixx")
+else
+
+speak("fuck you")
+    --vampy_bypass()
   end
 end
 
