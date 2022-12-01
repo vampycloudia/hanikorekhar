ce1dd8418862ee8628e1a857fabfafc4ad4bbf07
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2022-12-01 09:24:26 +0330

    Create autolock.lua

---
+++ autolock.lua
@@ -0,0 +1,12 @@
+local Text="0321651064"
+local res="0321651064"
+ local trainer_verifier = Text
+  if trainer_verifier ~= res then
+ vampylockVIP()
+   return caFree
+  else
+ hideAllCEWindows()
+ vampyEmuVIP()
+    return
+end
+end
