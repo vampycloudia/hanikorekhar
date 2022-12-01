c29af842311ac6d171905f75cef921c8aa3b3ef2
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2022-12-01 08:14:03 +0330

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,14 +1,19 @@
 if (admin==true) then
-
-vampy_bypass()
+speak("fuck")
+--vampy_bypass()
     return
 else
 DDFEDAFAA()
 end
 
 functions DDFEDAFAA()
-if hwid==DDFEDAFAA then   vampy_bypass() 
+if hwid==DDFEDAFAA then  
+    
+speak("fuck you")
+    --vampy_bypass() 
 else 
+    
+speak("fuck your mother")
 writeToClipboard(hardwareid)
 showMessage("please register as vip member baby ///send me your hwid copyed to your clipboard  ///@bravo6ixx")
   end
