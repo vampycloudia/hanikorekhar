587bb68a16d0808b311a6e35b3d34d75f9bae4da
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2022-12-01 08:07:45 +0330

    Create hwid.lua

---
+++ hwid.lua
@@ -0,0 +1,15 @@
+if (admin==true) then
+
+vampy_bypass()
+    return
+else
+DDFEDAFAA()
+end
+
+functions DDFEDAFAA()
+if hwid==DDFEDAFAA then   vampy_bypass() 
+else 
+writeToClipboard(Text)
+showMessage("please register as vip member baby ///send me your hwid copyed to your clipboard  ///@bravo6ixx")
+  end
+end
