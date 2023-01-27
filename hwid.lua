678d31264080b3ea8ab21ba8cdac297cf751ae99
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2023-01-27 08:00:54 +0330

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,11 +1,11 @@
 function user1()
-userhwid="DDFEDAFAB"
+userhwid="DDFEDAFAA"
  hwidres = all_trim(userhwid)
  hwidscaner = string.gsub(hwidres,'%A+','')
  local hwid_verifier = hardwareid
   if hwid_verifier ~= hwidscaner then
 writeToClipboard(hardwareid)
-showMessage("please register as vip member baby ///send me your hwid copyed to your clipboard  ///@bravo6ixx")
+showMessage("send me your HWid that was copyed to your clipboard  any question ??? mention me in our telegram group >> t.me/pubgvampy ///@bravo6ixx")
 else
     bypass()
   end
