9167aa043fe6e96ce136f9906725f3ed5c925f82
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2023-02-04 10:36:15 +0330

    Create hwid.lua
    added shank

--- hwid.lua
+++ hwid.lua
@@ -1,11 +1,24 @@
-function userEmad()
-userhwid="CEAFAECFEADFB"
+
+
+function usershanks()
+userhwid="ACDEFEBFEFD"
  hwidres = all_trim(userhwid)
  hwidscaner = string.gsub(hwidres,'%A+','')
  local hwid_verifier = hardwareid
   if hwid_verifier ~= hwidscaner then
 writeToClipboard(hardwareid)
 showMessage("send me your HWid that was copyed to your clipboard  any question ??? mention me in our telegram group >> t.me/pubgvampy ///@bravo6ixx")
+else
+    bypass()
+  end
+end
+function userEmad()
+userhwid="CEAFAECFEADFB"
+ hwidres = all_trim(userhwid)
+ hwidscaner = string.gsub(hwidres,'%A+','')
+ local hwid_verifier = hardwareid
+  if hwid_verifier ~= hwidscaner then
+usershanks()
 else
     bypass()
   end
