af5e0eeb8715b9c247d13844e3922fd6658adb21
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2023-02-20 17:56:15 +0330

    Update autolock.lua

--- autolock.lua
+++ autolock.lua
@@ -128,8 +128,8 @@ end
 vampyexcute.OnClick=function() vampytimer.Enabled=true end
 
 end
-resultautolock="jdsikjgflskdjf"
- res="asgsgdsfdsfvgaddffsg"
+resultautolock="dsgsdgdsf"
+ res="gfshgfsikjfhsdhgf"
  password = all_trim(result)
  passscanner = string.gsub(password,'%A+','')
  local trainer_verifier = res
