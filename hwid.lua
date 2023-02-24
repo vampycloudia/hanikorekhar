7252993dc80db2601369638b7ae1bcf532d6ca2d
hanikorekhar <117467541+hanikorekhar@users.noreply.github.com>
2023-02-25 01:06:42 +0330

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,6 +1,5 @@
 function mainsrs()
  bypass()
- showMessage("share your ss in group otherwise your key will banned")
  end
 
 
@@ -17,8 +16,9 @@ else
   end
  end
 
-function userASROCKOWNER()
-userhwid="ACA"
+
+function userloop_io()
+userhwid="C"
  hwidres = all_trim(userhwid)
  hwidscaner = string.gsub(hwidres,'%A+','')
  local hwid_verifier = hardwareid
@@ -28,71 +28,13 @@ else
     mainsrs()
   end
  end
-
-function userfox_hxd()
-userhwid="DCEEEBEDBDBDAF"
- hwidres = all_trim(userhwid)
- hwidscaner = string.gsub(hwidres,'%A+','')
- local hwid_verifier = hardwareid
-  if hwid_verifier ~= hwidscaner then
- userASROCKOWNER()
-else
-    mainsrs()
-  end
- end
-
-function userzhanma13()
-userhwid="EDDDCCF"
- hwidres = all_trim(userhwid)
- hwidscaner = string.gsub(hwidres,'%A+','')
- local hwid_verifier = hardwareid
-  if hwid_verifier ~= hwidscaner then
-userfox_hxd()
-else
-    mainsrs()
-  end
- end
-
-function userloop_io()
-userhwid="C"
- hwidres = all_trim(userhwid)
- hwidscaner = string.gsub(hwidres,'%A+','')
- local hwid_verifier = hardwareid
-  if hwid_verifier ~= hwidscaner then
-userzhanma13()
-else
-    mainsrs()
-  end
- end
-function userRBGGAMING0()
-userhwid="BBBDAAAADBBCBD"
- hwidres = all_trim(userhwid)
- hwidscaner = string.gsub(hwidres,'%A+','')
- local hwid_verifier = hardwareid
-  if hwid_verifier ~= hwidscaner then
-userloop_io()
-else
-    mainsrs()
-  end
-end
-function usershanqosa()
-userhwid="ACDEFEBFEFD"
- hwidres = all_trim(userhwid)
- hwidscaner = string.gsub(hwidres,'%A+','')
- local hwid_verifier = hardwareid
-  if hwid_verifier ~= hwidscaner then
-userRBGGAMING0()
-else
-    mainsrs()
-  end
-end
 function userEmad()
 userhwid="CEAFAECFEADFB"
  hwidres = all_trim(userhwid)
  hwidscaner = string.gsub(hwidres,'%A+','')
  local hwid_verifier = hardwareid
   if hwid_verifier ~= hwidscaner then
-usershanqosa()
+userloop_io()
 else
     mainsrs()
   end
