b58a78e53cbf787acb7d49818b2cf4701ab1806f
vampy々 <117467541+vampycloudia@users.noreply.github.com>
2025-12-29 15:04:40 +0330

    Update blogfa.theme.js

--- web/blogfa.theme.js
+++ web/blogfa.theme.js
@@ -1,7 +1,6 @@
-var cmt_caption = ["نظرات", "نظر بدهيد", "يک نظر", "نظر"];
-var cmt_blogid = "";
 var __cmt_updated = false;
 
+// رفرش کردن متن لینک نظرات برای هر پست
 function updatecomments() {
     var _cmts = [];
     var cnt = 0;
@@ -9,19 +8,23 @@ function updatecomments() {
     var url = "";
     var postid = 0;
 
-    if (cmt_blogid == "")
-        return;
+    if (!window.cmt_blogid) return;
 
     try {
-        for (c = 0; c < BlogComments.length; c += 2) {
+        if (typeof BlogComments === "undefined" || !BlogComments.length) {
+            return;
+        }
+
+        for (var c = 0; c < BlogComments.length; c += 2) {
             _cmts["_" + BlogComments[c]] = BlogComments[c + 1];
         }
 
         var allelements = document.getElementsByTagName("*");
-        for (i = 0; i < allelements.length; i++) {
+        for (var i = 0; i < allelements.length; i++) {
             postid = allelements[i].getAttribute("comment-for");
             if (postid != null) {
-                postid = parseInt(postid);
+                postid = parseInt(postid, 10);
+
                 if (_cmts["_" + postid] != null)
                     cnt = _cmts["_" + postid];
                 else
@@ -35,36 +38,35 @@ function updatecomments() {
                 url = "/comments/?blogid=" + cmt_blogid + "&postid=" + postid;
 
                 if (getwindowwidth() > 700)
-                    result = "<a href=\"javascript:void(0)\" onclick=\"javascript:window.open('" + url + "','blogfa_comments','status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no ,width=500px,height=500px')\" >" + result + " </a>";
+                    result =
+                        "<a href=\"javascript:void(0)\" onclick=\"javascript:window.open('" +
+                        url +
+                        "','blogfa_comments','status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no ,width=500px,height=500px')\" >" +
+                        result + " </a>";
                 else
                     result = "<a href=\"" + url + "\"  >" + result + " </a>";
 
                 allelements[i].innerHTML = result;
             }
         }
-    }
-    catch (e) {
-    }
+    } catch (e) {}
 
     __cmt_updated = true;
-} // end of updatecomments
+}
 
+// باز کردن آرشیو پیوندها
 function openlinks() {
     if (getwindowwidth() < 700)
         return true;
-    window.open('/links', 'blogfa_ld', 'status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no ,width=500px,height=500px');
+    window.open(
+        '/links',
+        'blogfa_ld',
+        'status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no ,width=500px,height=500px'
+    );
     return false;
 }
 
+// عرض پنجره
 function getwindowwidth() {
     return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 }
-
-if (window.addEventListener)
-    window.addEventListener("load", updatecomments);
-else if (window.attachEvent)
-    window.attachEvent("onload", updatecomments);
-
-setTimeout(function () {
-    if (__cmt_updated == false) updatecomments();
-}, 6000);
